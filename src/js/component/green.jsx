262531bfdd33a79d68643a2a68d99f0ce2ed1f63
Leonel Cedeño <52536063+Karel-CR@users.noreply.github.com>
2024-03-03 11:13:05 +0000

    Luz de trafico usando hooks y useState

---
+++ src/js/component/green.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const Green = () => {
+    return (
+        <div className="green-light"></div>
+    );
+}
+
+export default Green()
---
+++ src/js/component/yellow.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Yellow = () => {
+    return (
+        <div className="yellow-light"></div>
+    );
+}
+
+export default Yellow()
